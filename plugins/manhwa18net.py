e8f67229ae5d6e9ba35a5e5a603ab86c9550a3ce
gintokisakata918-prog <gintokisakata918@gmail.com>
2026-08-20 16:47:23 +0530

    Update manhwa18net.py

--- plugins/manhwa18net.py
+++ plugins/manhwa18net.py
@@ -255,20 +255,6 @@ class Manhwa18NetClient(MangaClient):
 
         return result
 
-            async def get_picture(self, manga_chapter, url, *args, **kwargs):
-        headers = {
-            **self.headers,
-            "Referer": manga_chapter.url,
-        }
-
-        kwargs["headers"] = headers
-
-        return await self.get_url(
-            url,
-            *args,
-            **kwargs
-        )
-
     async def search(
         self,
         query: str = "",
